8bb3e2a3fd18e8e5119eb55fad31e3acf8e5142e
Vaishnavi Rokade <rokadevaishnavi520@gmail.com>
2025-10-31 14:53:39 +0530

    Add files via upload

---
+++ Polymorphism.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void draw() {
+        cout << "Drawing Shape" << endl;
+    }
+};
+
+class Circle : public Shape {
+public:
+    void draw() override {
+        cout << "Drawing Circle" << endl;
+    }
+};
+
+int main() {
+    Shape* s = new Circle();
+    s->draw();
+    delete s;
+    return 0;
+}
