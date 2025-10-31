52068fe9ba7a20daeaec26edd231f95a7e1578df
Vaishnavi Rokade <rokadevaishnavi520@gmail.com>
2025-10-31 14:54:47 +0530

    Add files via upload

---
+++ Variables_DataTypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 22;
+    float salary = 25000.50;
+    char grade = 'A';
+    bool isActive = true;
+    string name = "Vaishnavi";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Salary: " << salary << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Active: " << isActive << endl;
+    return 0;
+}
